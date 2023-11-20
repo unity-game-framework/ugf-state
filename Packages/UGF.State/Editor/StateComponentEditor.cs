06a3f3a95018dd6d981897dc57ccb7e42a7145d8
Vorobey <vorobyovvorobyov@gmail.com>
2023-11-20 23:20:14 +0300

    Add editors

---
+++ Packages/UGF.State/Editor/StateComponentEditor.cs
@@ -0,0 +1,38 @@
+﻿using UGF.EditorTools.Editor.IMGUI;
+using UGF.EditorTools.Editor.IMGUI.Scopes;
+using UGF.State.Runtime;
+using UnityEditor;
+
+namespace UGF.State.Editor
+{
+    [CustomEditor(typeof(StateComponent), true)]
+    internal class StateComponentEditor : UnityEditor.Editor
+    {
+        private SerializedProperty m_propertyDefaultStateIndex;
+        private ReorderableListDrawer m_listStates;
+
+        private void OnEnable()
+        {
+            m_propertyDefaultStateIndex = serializedObject.FindProperty("m_defaultStateIndex");
+            m_listStates = new ReorderableListDrawer(serializedObject.FindProperty("m_states"));
+            m_listStates.Enable();
+        }
+
+        private void OnDisable()
+        {
+            m_listStates.Disable();
+        }
+
+        public override void OnInspectorGUI()
+        {
+            using (new SerializedObjectUpdateScope(serializedObject))
+            {
+                EditorIMGUIUtility.DrawScriptProperty(serializedObject);
+
+                EditorGUILayout.PropertyField(m_propertyDefaultStateIndex);
+
+                m_listStates.DrawGUILayout();
+            }
+        }
+    }
+}
